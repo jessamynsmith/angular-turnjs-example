af11be11f9192178dba381d8b251ff8c9901f055
Jessamyn Smith <geekchick77@gmail.com>
2017-02-15 13:41:57 -0700

    Connect the controller in the js rather than the html.

--- app/book/book.js
+++ app/book/book.js
@@ -2,32 +2,32 @@
 
 angular.module('angularTurnjsExample.book', ['ngRoute'])
 
-  .config(['$routeProvider', function($routeProvider) {
+  .config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/book', {
-      templateUrl: 'book/book.html'
-      // controller: 'BookCtrl'
+      templateUrl: 'book/book.html',
+      controller: 'bookCtrl'
     });
   }])
 
-    .controller('bookCtrl', function ($scope) {
+  .controller('bookCtrl', function ($scope) {
 
-    })
+  })
 
-// External controller 1
-    .controller('externalCtrl1', function ($scope) {
-        $scope.myName = 'value from EXTERNAL controller 1';
-        $scope.name = 'Ronaldinho';
-        $scope.pic = 'book/ronaldino.jpg';
-        $scope.biography = "Ronaldo de Assis Moreira (born 21 March 1980), commonly known as Ronaldinho, is a Brazilian professional footballer and current club ambassador for Spanish club FC Barcelona after signing for them in September 2016.";
+  // External controller 1
+  .controller('externalCtrl1', function ($scope) {
+    $scope.myName = 'value from EXTERNAL controller 1';
+    $scope.name = 'Ronaldinho';
+    $scope.pic = 'book/ronaldino.jpg';
+    $scope.biography = "Ronaldo de Assis Moreira (born 21 March 1980), commonly known as Ronaldinho, is a Brazilian professional footballer and current club ambassador for Spanish club FC Barcelona after signing for them in September 2016.";
 
-        this.childExternalCtrlMethod = function () {
-            console.log("parent just called child's excternal  ctrl 1 Method");
-        }
-    })
-    // External controller 2
-    .controller('externalCtrl2', function ($scope) {
-        $scope.myName = 'value from EXTERNAL controller 2';
-        this.childExternalCtrlMethod = function () {
-            console.log("parent just called child's excternal ctrl 2 Method");
-        }
-    });
+    this.childExternalCtrlMethod = function () {
+      console.log("parent just called child's excternal  ctrl 1 Method");
+    }
+  })
+  // External controller 2
+  .controller('externalCtrl2', function ($scope) {
+    $scope.myName = 'value from EXTERNAL controller 2';
+    this.childExternalCtrlMethod = function () {
+      console.log("parent just called child's excternal ctrl 2 Method");
+    }
+  });
