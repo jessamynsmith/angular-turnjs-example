43931bebc5545dc619611597349167972cdef740
jessamynsmith <jessamyn.smith@gmail.com>
2017-07-24 13:26:26 -0400

    Fixed spelling errors and package.json syntax error.

--- app/book/book.js
+++ app/book/book.js
@@ -21,13 +21,13 @@ angular.module('angularTurnjsExample.book', ['ngRoute'])
     $scope.biography = "Ronaldo de Assis Moreira (born 21 March 1980), commonly known as Ronaldinho, is a Brazilian professional footballer and current club ambassador for Spanish club FC Barcelona after signing for them in September 2016.";
 
     this.childExternalCtrlMethod = function () {
-      console.log("parent just called child's excternal  ctrl 1 Method");
+      console.log("parent just called child's external ctrl 1 Method");
     };
   })
   // External controller 2
   .controller('externalCtrl2', function ($scope) {
     $scope.myName = 'value from EXTERNAL controller 2';
     this.childExternalCtrlMethod = function () {
-      console.log("parent just called child's excternal ctrl 2 Method");
+      console.log("parent just called child's external ctrl 2 Method");
     };
   });
